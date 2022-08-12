35a50b54a25ec6450e5aba95e624d2918be9d67d
Adauto <you@example.com>
2022-08-12 15:37:10 -0300

    exercicio 77

--- exercicio_77_tuplas.py
+++ exercicio_77_tuplas.py
@@ -3,3 +3,14 @@ Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras
 (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
 '''
+
+words = ('Praticar', 'Produzir', 'Procastinar', 'Dormir', 'Comer', 'Programar', 'Chorar')
+
+for word in words:
+
+    print('Na palavra {} temos as vogais:'.format(word), end = ' ')
+
+    for letter in word: # para cada letra da palavra vou checar se é vogal
+        if letter.strip().lower() in 'aeiou':
+            print(letter, end = ' ')
+    print('\n')
